439c11e0b59794cb00880cfef9a528f017257406
Jacob Bonde <mail@jacobbonde.dk>
2019-12-01 12:12:14 +0100

    Day 1

---
+++ Day1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC19
+{
+    public class Day1
+    {
+        public static void Question1(int[] masses)
+        {
+            int result = 0;
+
+            foreach (var mass in masses)
+            {   
+                result += GetFuel(mass);
+            }
+
+            Console.WriteLine($"QUESTION 1: Fuel need: {result}");
+        }
+        public static void Question2(int[] masses)
+        {
+            int result = 0;
+
+            foreach (var mass in masses)
+            {   
+                result += GetFuelRecursively(mass);
+            }
+
+            Console.WriteLine($"QUESTION 2: Fuel need: {result}");
+        }
+
+        private static int GetFuel(int mass)
+        {
+            return mass/3-2;
+        }
+
+        private static int GetFuelRecursively(int mass)
+        {
+            int fuel = mass/3-2;
+
+            if(fuel > 0) {
+                return fuel + GetFuel(fuel);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2019");
+
+            Day1();
+            Day2();
+        }
+    
+        public static void Day1()
+        {
+            int[] masses = ReadInputFile("day1-input.txt");
+            AoC19.Day1.Question1(masses);
+            AoC19.Day1.Question2(masses);
+        }
+
+        public static void Day2()
+        {
+            int[] input = ReadInputFile("day2-input.txt");
+            AoC19.Day2.Question1(input);
+            AoC19.Day2.Question2(input);
+        }
+
+        public static int[] ReadInputFile(string filename)
+        {
+            return File.ReadLines(filename).Select(l => int.Parse(l)).ToArray();
+        }
+    }
+}
