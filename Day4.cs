aeb29977dcd3b489f3767c83aec7ba3bf966f077
Jacob Bonde Larsen <jbl@siteimprove.com>
2019-12-03 11:03:53 +0100

    Ready for day 4

---
+++ Day4.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+
+namespace AoC19
+{
+    internal class Day4
+    {
+        public static void Part1(string[] input)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            sw.Stop();
+
+            Console.WriteLine("PART 1");
+            Console.WriteLine($"\t Answer:");
+            Console.WriteLine($"\t Time: {sw.ElapsedMilliseconds}");
+        }
+
+        public static void Part2(string[] input)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+            sw.Stop();
+
+            Console.WriteLine("PART 2");
+            Console.WriteLine($"\t Answer: {}");
+            Console.WriteLine($"\t Time: {sw.ElapsedMilliseconds}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -53,5 +53,26 @@ namespace AoC19
             AoC19.Day3.Part2(input1, input2);
             Console.WriteLine();
         }
+
+        public static void Day4()
+        {
+            Console.WriteLine("##### DAY 4 #####");
+            string[] input = Input.ReadStringLines("day4-input.txt");
+            // string[] input1 = input[0].Split(',');
+            // string[] input2= input[1].Split(',');
+
+            //  string[] input1 = new[] {"R8","U5","L5","D3"};
+            //  string[] input2 = new[] {"U7","R6","D4","L4"};
+
+            // string[] input1 = new[] {"R75","D30","R83","U83","L12","D49","R71","U7","L72"};
+            // string[] input2 = new[] {"U62","R66","U55","R34","D71","R55","D58","R83"};
+
+            //  string[] input1 = new[] {"R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"};
+            //  string[] input2 = new[] {"U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"};
+
+            AoC19.Day4.Part1(input);
+            AoC19.Day4.Part2(input);
+            Console.WriteLine();
+        }
     }
 }
